7dbc7dd5ea5c18b1f5c28d9080086df599846090
Hayden <yangxinhust@hotmail.com>
2017-09-02 22:24:39 +0800

    t2

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 git add -f .
 git remote rm origin
-git remote add origin https://Hsiny:a812f841fdcd8dca9576519e2cae32da538e3d38@github.com/Hsiny/Shadowrocket-Config.git
+git remote add origin https://Hsiny:b49d0b6de1032727d0738c108f6fdb1df24691d8@github.com/Hsiny/Shadowrocket-Config.git
 git add -A
 git commit -m "Dayly Update $TRAVIS_BUILD_NUMBER pushed [skip ci] "
 git push -fq origin master
