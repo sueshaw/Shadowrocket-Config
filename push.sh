763baf9ed547a5cbc92e47c260df8340bf8cd443
Hayden <yangxinhust@hotmail.com>
2017-09-02 22:08:50 +0800

    test

--- push.sh
+++ push.sh
@@ -1,21 +1,8 @@
 #!/bin/sh
-
-setup_git() {
-  git config --global user.email "yangxinhust@hotmail.com"
-  git config --global user.name "Hayden"
-}
-
-commit_website_files() {
-  git add -A
-  git commit --message "Daily Update [skip travis]"
-}
-
-upload_files() {
-  git remote rm origin
-  git remote add origin https://${GH_TOKEN}@github.com/Hsiny/Shadowrocket-Config.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin master
-}
-
-setup_git
-commit_website_files
-upload_files
+git add -f .
+git remote rm origin
+git remote add origin https://Hsiny:$GH_TOKEN@github.com/Hsiny/Shadowrocket-Config.git
+git add -f .
+git commit -m "Dayly Update $TRAVIS_BUILD_NUMBER pushed [skip ci] "
+git push -fq origin master > /dev/null
+echo -e "Done\n"
