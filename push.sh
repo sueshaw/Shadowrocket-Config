aa76848508d7f9d1ac2f32a5ffa874956acf8f54
Hayden <yangxinhust@hotmail.com>
2017-09-02 22:29:35 +0800

    t3

--- push.sh
+++ push.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+git config --global user.email "yangxinhust@hotmail.com"
+git config --global user.name "Hsiny"
 git add -f .
 git remote rm origin
 git remote add origin https://Hsiny:b49d0b6de1032727d0738c108f6fdb1df24691d8@github.com/Hsiny/Shadowrocket-Config.git
