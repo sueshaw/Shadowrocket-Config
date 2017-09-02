b1957682e2fb7eb0b0bde87a4d271638047649b0
Hayden <yangxinhust@hotmail.com>
2017-09-02 22:34:50 +0800

    t5

--- push.sh
+++ push.sh
@@ -3,8 +3,8 @@ git config --global user.email "yangxinhust@hotmail.com"
 git config --global user.name "Hsiny"
 git add -f .
 git remote rm origin
-git remote add origin https://Hsiny:31f693fad67ca6eaa5409a196fd9f6349041446f@github.com/Hsiny/Shadowrocket-Config.git
+git remote add origin https://Hsiny:${TT}@github.com/Hsiny/Shadowrocket-Config.git
 git add -A
 git commit -m "Dayly Update $TRAVIS_BUILD_NUMBER pushed [skip ci] "
 git push -fq origin master
-echo -e "Done\n"
+echo -e "Done\n ${TT}"
